31cf66deb52899d7e4bce71c4f4c5b78d1933671
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-04-30 04:33:30 -0400

    debug

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://replit.com',
     'https://replit.com/@DaveH93/inf653finalproject',
+    'https://dazzling-snickerdoodle-777101.netlify.app/',
     'http://127.0.0.1:5500',
     'http://localhost:3500'
 ];
